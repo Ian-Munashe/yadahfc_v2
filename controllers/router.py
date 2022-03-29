414c352ccfd24ac987b7557b9516a528215f4b8e
Ian-Munashe <macmushe@gmail.com>
2022-03-29 17:53:11 +0200

    Update commit

--- controllers/router.py
+++ controllers/router.py
@@ -7,17 +7,33 @@ class Router(FlaskView):
     def index(self):
         return render_template('pages/index.html', title = 'Yadah Football Club')
 
-    @route('/upcoming-match')
-    def upcoming_match(self):
-        return render_template('pages/upcoming-match.html', title = 'Upcoming Match')
+    @route('/about')
+    def about(self):
+        return render_template('pages/about.html', title = 'About The Club')
 
-    @route('/match-results')
-    def match_results(self):
-        return render_template('pages/match-results.html', title = 'Match Results')
+    @route('/team')
+    def team(self):
+        return render_template('pages/team.html', title = 'Our Team')
 
-    @route('/match-details')
-    def match_details(self):
-        return render_template('pages/match-details.html', title = 'Match Details')
+    @route('/gallery')
+    def gallery(self):
+        return render_template('pages/gallery.html', title = 'Gallery')
+
+    @route('/fixtures')
+    def fixtures(self):
+        return render_template('pages/fixtures.html', title = 'Fixtures')
+
+    # @route('/upcoming-match')
+    # def upcoming_match(self):
+    #     return render_template('pages/upcoming-match.html', title = 'Upcoming Match')
+
+    # @route('/match-results')
+    # def match_results(self):
+    #     return render_template('pages/match-results.html', title = 'Match Results')
+
+    # @route('/match-details')
+    # def match_details(self):
+    #     return render_template('pages/match-details.html', title = 'Match Details')
 
     @route('/contact')
     def contact(self):
